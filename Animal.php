b78eccbb5a8f74c66ad47630591f03b9f1638ddb
izanlopezbc <izan.lopez@insbaixcamp.cat>
2025-03-28 19:08:24 +0100

    Nueva clase Animal

---
+++ Animal.php
@@ -0,0 +1,10 @@
+<?php
+class Animal {
+    private $nom;
+    private $edat;
+
+    public function __construct($nomP, $edatP) {
+        $this->nom = $nomP;
+        $this->edat = $edatP;
+    }
+}
